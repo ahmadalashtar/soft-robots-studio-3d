35dacad915a7382bef41e9b6dced7b97508927d4
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-07-12 11:03:40 +0300

    calculate angle between 2D vectors

--- calculateFitness3D.m
+++ calculateFitness3D.m
@@ -184,44 +184,7 @@ function [rotx, roty] = calculateLastAngle(robot_points, ee_index, target)
     
 end
 
-% input: 
-% - v1: target's projection on zx or zy
-% - v2: end point's vector projection on zx or zy
-% output:
-% - angle: the difference in angle to go from v2 to v1
 
-function angle = calculateAngle(v1,v2)
-    
-    x1 = v1(1);
-    y1 = v1(2);
-    x2 = v2(1);
-    y2 = v2(2);
-    v1n = norm(v1);
-    v2n = norm(v2);
-    
-    if (x1 >= 0)
-        v1angle = asind(y1/v1n);
-    elseif (y1<=0)
-        v1angle = asind(y1/v1n);
-        v1angle = -180 - v1angle ;
-    else
-        v1angle = asind(y1/v1n);
-        v1angle = 180 - v1angle ;
-    end
-    
-    if (x2 >= 0)
-        v2angle = asind(y2/v2n);
-    elseif (y2<=0)
-        v2angle = asind(y2/v2n);
-        v2angle = -180 - v2angle ;
-    else
-        v2angle = asind(y2/v2n);
-        v2angle = 180 - v2angle ;
-    end
-    
-    angle = v1angle - v2angle;
-    angle = round(angle,3);
-end
 
 % % Calculate the area of a triangle given its three vertices
 % function [area] = calculateTriangleArea(p1,p2,p3)
