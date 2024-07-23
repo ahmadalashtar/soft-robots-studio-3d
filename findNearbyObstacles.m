e51a9c0a2a4fe4ec5414dc2a02fac2468b947453
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-07-23 10:38:25 +0300

    bug fix norm of entire vector

--- findNearbyObstacles.m
+++ findNearbyObstacles.m
@@ -3,7 +3,7 @@ function [nearby_obstacle_indices] = findNearbyObstacles(ee, link_length, min_le
     nearby_obstacle_indices = zeros(1,n_obstacles);
     n_nearby_obstacles = 0;
     for i=1:1:n_obstacles
-        d = norm(ee-obstacles(i,1:2)) - (obstacles(i,4) + min_length);
+        d = norm(ee-obstacles(i,1:3)) - (obstacles(i,4) + min_length);
         if d<link_length
             n_nearby_obstacles = n_nearby_obstacles +1;
             nearby_obstacle_indices(n_nearby_obstacles) = i;
