c351012377ea429adb091f3672f525d6345a5499
Ozi6 <magnusterrarian@gmail.com>
2024-07-23 09:42:31 +0300

    angles2UnitVector Name Changed
    -Name of function angles2UnitVector changed to segment2UnitVector.

--- segment2UnitVector.m
+++ segment2UnitVector.m
@@ -1,3 +1,3 @@
-function [ u ] = angles2UnitVector(target,end_point)  
+function [ u ] = segment2UnitVector(target,end_point)  
     u = (target(1:3)-end_point(1:3))/norm(target(1:3)-end_point(1:3));
 end
