dc6974f44fe99f3b8adce2dd0da2183847cbefe1
Ozi6 <magnusterrarian@gmail.com>
2024-07-23 15:26:18 +0300

    blendValues Work Method Fixed
    -blendValues now does blending for everything.

--- blendValues.m
+++ blendValues.m
@@ -1,30 +1,18 @@
-function [gene_cX, gene_cY] = blendValues(gene_p1X, gene_p2X, gene_p1Y, gene_p2Y, alpha, bounds, isInteger)
-    if gene_p1X < gene_p2X
-        minGeneX = gene_p1X;
-        maxGeneX = gene_p2X;
+function [gene_c] = blendValues(gene_p1, gene_p2, alpha, bounds, isInteger)
+    if gene_p1 < gene_p2
+        minGene = gene_p1;
+        maxGene = gene_p2;
     else
-        minGeneX = gene_p2X;
-        maxGeneX = gene_p1X;
-    end
-    if gene_p1Y < gene_p2Y
-        minGeneY = gene_p1Y;
-        maxGeneY = gene_p2Y;
-    else
-        minGeneY = gene_p2Y;
-        maxGeneY = gene_p1Y;
+        minGene = gene_p2;
+        maxGene = gene_p1;
     end
     u = rand();
     gamma = (1+2*alpha)*u-alpha;
-
-    gene_cX = (1-gamma)*minGeneX + gamma*maxGeneX;
-    gene_cX = max(min(bounds(2),gene_cX),bounds(1));
-
-    gene_cY = (1-gamma)*minGeneY + gamma*maxGeneY;
-    gene_cY = max(min(bounds(2),gene_cY),bounds(1));
+    gene_c = (1-gamma)*minGene + gamma*maxGene;
+    gene_c = max(min(bounds(2),gene_c),bounds(1));
     
     % if the gene is an integer number, transform it
     if isInteger==true
-        gene_cX = nearest(gene_cX);
-        gene_cY = nearest(gene_cY);
+        gene_c = nearest(gene_c);
     end    
 end
