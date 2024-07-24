d1980ccb51016f98bc8fadc701ac1418b23aa789
Ozi6 <magnusterrarian@gmail.com>
2024-07-24 14:10:05 +0300

    proportionate Made
    -File proportionate ported.

---
+++ proportionate.m
@@ -0,0 +1,30 @@
+function [matPool] = proportionate(fit_array, isMin)
+    global gas; % genetic algorithm settings
+    
+    if isMin == true
+        fit_array(:,1) = flip(fit_array(:,1));  % for minimization, rank must be flipped
+    end
+    s = sum(fit_array(:,1));
+    
+    % calculate probability
+    for i = 1:1:gas.n_individuals
+        if i==1
+            fit_array(i,1) = fit_array(i,1)/s;
+        else
+            fit_array(i,1) = fit_array(i,1)/s + fit_array(i-1,1);
+        end
+    end
+    
+    % fill mating pool
+    matPool = zeros(gas.n_individuals,1);
+    for i = 1:1:gas.n_individuals
+        r = rand();
+        for j = 1:1:gas.n_individuals
+            if fit_array(j,1) >= r
+                matPool(i) = fit_array(j,2);
+                break;
+            end
+        end
+    end
+    
+end
