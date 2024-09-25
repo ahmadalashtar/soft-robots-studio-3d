fe79014f82f0ef93f6ca2502477f5d4ed54c3754
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-09-25 16:06:27 +0300

    complete segment intersects circle

--- collisionCheck.m
+++ collisionCheck.m
@@ -1,19 +1,21 @@
 function intersections = collisionCheck(conf)
     global op;
-    intersections = 0;
+    
+    obstacles = op.obstacles;
+    nObstacles = size(obstacles,1);
+
+    nodes = solveForwardKinematics3D(conf,op.home_base,0);
+
     for i = 1:size(conf,1)
         if conf(i,3)==0
             nUsedLinks = i-1;
             break;
         end
     end
-
-    nodes = solveForwardKinematics3D(conf,op.home_base,0);
-    nUsedNodes = nUsedLinks + 1;
     
-    obstacles = op.obstacles;
-    nObstacles = size(obstacles,1);
+    nUsedNodes = nUsedLinks + 1;
 
+    intersections = 0;
     for i = 1 : nUsedNodes - 1 
         for j = 1 : nObstacles
             intersections = intersections + doTheyIntersect(nodes(i,:),nodes(i+1,:),obstacles(j,:));
@@ -26,5 +28,16 @@ function intersect = doTheyIntersect(startPt,endPt,obstacle)
 end
 
 function intersects = segmentIntersectsCircle(segment,circle)
+    point1 = segment(1,:);
+    point2 = segment(2,:);
+    center = circle(1:2);
+    centerToSegmentDistance = pointToSegment3D(center,point1,point2);
 
+    radius = circle(3);
+    
+    if centerToSegmentDistance > radius
+        intersects = false;
+    else
+        intersects = true;
+    end
 end
