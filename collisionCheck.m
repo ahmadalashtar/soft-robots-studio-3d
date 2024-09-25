92c18fae76ad54a7b31b4adf93f9144e4d75272c
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-09-25 15:45:07 +0300

    nested loop for nodes and obstacles to stack up intersections

--- collisionCheck.m
+++ collisionCheck.m
@@ -8,7 +8,17 @@ function intersections = collisionCheck(conf)
         end
     end
 
+    nodes = solveForwardKinematics3D(conf,op.home_base,0);
+    nUsedNodes = nUsedLinks + 1;
+    
+    obstacles = op.obstacles;
+    nObstacles = size(obstacles,1);
 
+    for i = 1 : nUsedNodes - 1 
+        for j = 1 : nObstacles
+            intersections = intersections + doIntersect(nodes(i,:),nodes(i+1,:),obstacles(j,:));
+        end
+    end
 end
 
 function intersect = doIntersect(startPt,endPt,obstacle)
