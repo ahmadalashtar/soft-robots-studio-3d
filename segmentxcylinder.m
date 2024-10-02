0567d740dd921d9d6bb2df5d0c50c0ddb3a94994
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-10-02 15:58:01 +0300

    include inside as first case

--- segmentxcylinder.m
+++ segmentxcylinder.m
@@ -1,4 +1,10 @@
 function intersect = segmentxcylinder(startPt,endPt,obstacle)
+
+    inside = segmentInsideCylinder(startPt,endPt,obstacle);
+    if inside
+        intersect = true;
+        return;
+    end
     intersectXY = planarIntersection(startPt,endPt,obstacle,"XY");
     if ~intersectXY
         intersect = false;
@@ -49,12 +55,7 @@ function intersect  = planarIntersection(startPt,endPt,obstacle,plane)
     upperVertex1 = [obstacleX-radius, obstacleY-height];
     upperVertex2 = [obstacleX+radius, obstacleY-height];
 
-    inside = lineInsideRectangle(startPoint,endPoint,[upperVertex1; upperVertex2; lowerVertex2; lowerVertex1]);
-
-    if inside
-        intersect = true;
-        return;
-    end
+    
     
     [x1out, ~] = polyxpoly([startPoint(1), endPoint(1)],[startPoint(2),endPoint(2)],...
         [lowerVertex1(1), upperVertex1(1)],[lowerVertex1(2), upperVertex1(2)]);
@@ -84,6 +85,37 @@ function intersects = segmentIntersectsCircle(segment,circle)
     else
         intersects = true;
     end
+end
+
+function inside = segmentInsideCylinder(startPt,endPt,obstacle)
+radius = obstacle(4);
+height= obstacle(5);
+obstacleX = obstacle(2);
+obstacleY = obstacle(3);
+startPoint = [startPt(2) startPt(3)];
+endPoint = [endPt(2) endPt(3)];
+
+lowerVertex1 = [obstacleX-radius, obstacleY];
+lowerVertex2 = [obstacleX+radius, obstacleY];
+upperVertex1 = [obstacleX-radius, obstacleY-height];
+upperVertex2 = [obstacleX+radius, obstacleY-height];
+
+insideYZ = lineInsideRectangle(startPoint,endPoint,[upperVertex1; upperVertex2; lowerVertex2; lowerVertex1]);
+
+
+obstacleX = obstacle(1);
+obstacleY = obstacle(3);
+startPoint = [startPt(1) startPt(3)];
+endPoint = [endPt(1) endPt(3)];
+lowerVertex1 = [obstacleX-radius, obstacleY];
+lowerVertex2 = [obstacleX+radius, obstacleY];
+upperVertex1 = [obstacleX-radius, obstacleY-height];
+upperVertex2 = [obstacleX+radius, obstacleY-height];
+
+insideXZ = lineInsideRectangle(startPoint,endPoint,[upperVertex1; upperVertex2; lowerVertex2; lowerVertex1]);
+
+inside = insideXZ && insideYZ;
+
 end
 
 function inside = lineInsideRectangle(startPoint,endPoint,rectangle)
