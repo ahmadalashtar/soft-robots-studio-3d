c9d2df0233455772ac60b52e4388e56f2a0accd4
Badallocoredumped <emirozen57@hotmail.com>
2024-10-23 15:32:00 +0300

    Finished Experiment Main
    -Added 3 more parameters to the fitness array to represent the combination of the parameters.
    -Fixed DE algorithm bug

--- iteration.m
+++ iteration.m
@@ -1,4 +1,4 @@
-function iteration(numOfIteration,count,TaskID,k)
+function iteration(numOfIteration,count,TaskID,k,param)
     global eas
     global op
     global result
@@ -6,12 +6,15 @@ function iteration(numOfIteration,count,TaskID,k)
         tStart = tic;
         [best_chrom config fit_array]=main(1);
         tEnd = toc(tStart);
-        fit_array(1,eas.fitIdx.parameterID) = 1;
         fit_array(1,eas.fitIdx.algo) = k;
         fit_array(1,eas.fitIdx.runTime) = tEnd;
         fit_array(1,eas.fitIdx.taskID) = TaskID;
         fit_array(1,eas.fitIdx.runID) = i;
         fit_array(1,eas.fitIdx.chromID) = count;
+        fit_array(1,eas.fitIdx.parameterInd1) = param(1);
+        fit_array(1,eas.fitIdx.parameterInd2) = param(2);
+        fit_array(1,eas.fitIdx.parameterInd3) = param(3);
+        
         
         result.output_matrix(count,:) = fit_array(1, :);
         result.chromosome_mat{count,1} = best_chrom;
