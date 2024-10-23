66ba0d87498bd4f0144a2854b586c4611ee75851
Ahmad Alashtar <ahmadalashtar@gmail.com>
2024-10-23 14:17:21 +0300

    new undulation done

--- calculateUndulation.m
+++ calculateUndulation.m
@@ -1,10 +1,78 @@
-function [und] = calculateUndulation(chrom)
-
-    global op;  % optimization problem
+function [undulations] = calculateUndulation(chrom)
+    global op;
     global eas;  % genetic algorithm settings
-    
-    n_targets = size(op.targets,1);
+
     n_links = size(chrom,2) - eas.extra_genes;
+    configurations = decodeIndividual(chrom);
+
+    undulations = 0;
+
+    for i = 1 : size(configurations,3)
+        conf = configurations(:,:,i);
+        nodes = solveForwardKinematics3D(conf,op.home_base,0);
+        lastNode = chrom(i*2-1, n_links + 1);
+        
+        if lastNode > 2
+            signsX = zeros(1,lastNode-2);
+            signsY = zeros(1,lastNode-2);
+            for j = 1 : lastNode-3
+                point1 = nodes(j,:);
+                point2 = nodes(j+1,:);
+                point3 = nodes(j+2,:);
+                vector1 = point2-point1;
+                vector2 = point3-point2;
+
+                % normalize
+                vector1 = vector1/norm(vector1);
+                vector2 = vector2/norm(vector2);
+
+                signsX(j) = getRotationSign(vector1(2:3),vector2(2:3));
+                % reversed so that it's calculated the same, to understand
+                % this, darw the rotation axes on a paper.
+                signsY(j) = getRotationSign([vector1(3) vector1(1)],[vector1(3) vector1(1)]);
+            end
+
+            % check if the first sign is positive, negative, or zero
+            for j = 1:size(signs,2)-1
+                if signs(j)~=signs(j+1) 
+                    undulations = undulations + 1;
+                end
+            end
+        end
+    end
+
+end
+
+% 2D vectors, could be [z x] and could be [y z]
+% sign is +, -, or 0
+function [sign] = getRotationSign(vector1,vector2)
+    % coordinate axis has 4 parts, upper right, upper left, lower right,
+    % lower left
+    
+    x1 = vector1(1);
+    x2 = vector2(1);
+    y1 = vector1(2);
+    y2 = vector2(2);
+    
+
+    % first case: no change
+    if y1 == y2 && x1 == x2
+        sign = 0;
+        return;
+    end
+
+    angle1 = atan2(x1,y1);
+    angle2 = atan2(x2,y2);
+    
+    difference = angle1 - angle2;
+    sign = (difference/difference)*(-1);
+end
+
+
+
+
+    
+    
     
     changes_x = zeros(n_targets,1);       % count changes in direction 
     changes_y = zeros(n_targets,1);       % count changes in direction
