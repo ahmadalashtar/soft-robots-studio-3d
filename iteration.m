51d161e7273b0e9ec57eb46e2a6a01ecf7fe0ec0
Erk Demirel <123633346+Sourchax@users.noreply.github.com>
2024-11-08 17:03:25 +0300

    Major Fix
    -Fixed the saving/loading algorithm
    -Added proper commetns and percentage calculation
    -Shows time elapsed for each algorithm

--- iteration.m
+++ iteration.m
@@ -1,8 +1,8 @@
-function iteration(numOfIteration,count,TaskID,k,param,nameOfFile)
+function iteration(TaskID,k,param,nameOfFile)
     global eas
     global op
     global result
-    for i=1:numOfIteration
+    for i=eas.currIteration:result.settings.numOfIteration
         tStart = tic;
         [best_chrom config fit_array]=main(1);
         tEnd = toc(tStart);
@@ -10,14 +10,15 @@ function iteration(numOfIteration,count,TaskID,k,param,nameOfFile)
         fit_array(1,eas.fitIdx.runTime) = tEnd;
         fit_array(1,eas.fitIdx.taskID) = TaskID;
         fit_array(1,eas.fitIdx.runID) = i;
-        fit_array(1,eas.fitIdx.chromID) = count;
+        fit_array(1,eas.fitIdx.chromID) = eas.count;
         fit_array(1,eas.fitIdx.parameterInd1) = param(1);
         fit_array(1,eas.fitIdx.parameterInd2) = param(2);
         fit_array(1,eas.fitIdx.parameterInd3) = param(3);
         
-        result.output_matrix(count,:) = fit_array(1, :);
-        result.chromosome_mat{count,1} = best_chrom;
-        count = count + 1;
+        result.output_matrix(eas.count,:) = fit_array(1, :);
+        result.chromosome_mat{eas.count,1} = best_chrom;
+        eas.count = eas.count + 1;
+        result.settings = eas;
         save(nameOfFile,'result');
     end
 end
